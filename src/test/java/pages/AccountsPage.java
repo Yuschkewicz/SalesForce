49969cd214579eedbd8b84af9e8afe2e2ae15794
Yuschkewicz <13and.jei@gmail.com>
2022-04-05 01:04:12 +0300

    0.2

--- src/test/java/pages/AccountsPage.java
+++ src/test/java/pages/AccountsPage.java
@@ -4,11 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import java.time.Duration;
+import static org.testng.Assert.assertEquals;
 
 public class AccountsPage extends BasePage {
-
-
 
     public AccountsPage(WebDriver driver) {
         super(driver);
@@ -20,18 +18,38 @@ public class AccountsPage extends BasePage {
         driver.findElement(USER_NAME).sendKeys("13and.jei-kd2q@force.com");
         driver.findElement(PASSWORD_INPUT).sendKeys("minsk1985");
         driver.findElement(By.id("Login")).submit();
-        wait.until((ExpectedConditions.visibilityOfElementLocated(By.className("uiImage"))));
+        wait.until((ExpectedConditions.visibilityOfElementLocated(By.id("trial-customdemo"))));
+
     }
 
-    public void accountCreate() {
+    public void invalidLogin() {
+        driver.get(baseUrl);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(USER_NAME));
+        driver.findElement(USER_NAME).sendKeys("13and.jei-kd12q@force.com");
+        driver.findElement(PASSWORD_INPUT).sendKeys("minsk1985");
+        driver.findElement(By.id("Login")).submit();
+        String notification = driver.findElement(By.id("error")).getText();
+        assertEquals(notification, "Please check your username and password." +
+                " If you still can't log in, contact your Salesforce administrator.");
+    }
 
-        driver.get(baseUrl + "lightning/o/Account/list?filterName=Recent");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    public void invalidPassword(){
+        driver.get(baseUrl);
         wait.until(ExpectedConditions.visibilityOfElementLocated(USER_NAME));
         driver.findElement(USER_NAME).sendKeys("13and.jei-kd2q@force.com");
-        driver.findElement(PASSWORD_INPUT).sendKeys("minsk1985");
-        driver.findElement(By.id("Login")).click();
-        driver.findElement(By.className("forceActionLink")).click();
+        driver.findElement(PASSWORD_INPUT).sendKeys("minsk1981");
+        driver.findElement(By.id("Login")).submit();
+        String alert=driver.findElement(By.id("error")).getText();
+        assertEquals(alert,"Please check your username and password. " +
+                "If you still can't log in, contact your Salesforce administrator.");
+
+
+    }
+    public void accountCreate() {
+        driver.get("https://tsw17.lightning.force.com/lightning/o/Account/list?filterName=Recent");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class," +
+                "'slds-breadcrumb__item')]//span[text()='Accounts']")));
+        driver.findElement(By.cssSelector("a[title=New]")).click();
 
 
     }
---
+++ src/test/java/pages/NewAccountModal.java
@@ -0,0 +1,61 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import wrappers.DropDown;
+import wrappers.Input;
+import wrappers.TextArea;
+
+public class NewAccountModal extends BasePage {
+
+
+    public NewAccountModal(WebDriver driver) {
+        super();
+        this.driver = driver;
+    }
+
+
+    public void createInputFields(String accountName, String webSite, String phone, String fax, String parent_account,
+                                  String employees, String annual_revenue, String billing_city,
+                                  String billing_state, String shipping_city, String shipping_state,
+                                  String billing_zip, String billing_country, String shipping_zip,
+                                  String shipping_country) {
+        new Input(driver, "Account Name").write(accountName);
+        new Input(driver, "Website").write(webSite);
+        new Input(driver, "Phone").write(phone);
+        new Input(driver, "Fax").write(fax);
+        new Input(driver, "Parent Account").write(parent_account);
+        new Input(driver, "Employees").write(employees);
+        new Input(driver, "Annual Revenue").write(annual_revenue);
+        new Input(driver, "Billing City").write(billing_city);
+        new Input(driver, "Billing State/Province").write(billing_state);
+        new Input(driver, "Shipping City").write(shipping_city);
+        new Input(driver, "Shipping State/Province").write(shipping_state);
+        new Input(driver, "Billing Zip/Postal Code").write(billing_zip);
+        new Input(driver, "Billing Country").write(billing_country);
+        new Input(driver, "Shipping Zip/Postal Code").write(shipping_zip);
+        new Input(driver, "Shipping Country").write(shipping_country);
+        driver.findElement(By.xpath("//span[text()='Copy Billing Address to Shipping Address']" +
+                "/ancestor::div[contains(@class, 'uiInput')]//input")).click();
+
+
+    }
+
+    public void createDropDownFields(String type, String industry) {
+        new DropDown(driver, "Type").select(type);
+        new DropDown(driver, "Industry").select(industry);
+
+    }
+
+    public void createTextAreaFields(String description, String billing_street, String shipping_street) {
+        new TextArea(driver, "Description").textAreaWrite(description);
+        new TextArea(driver, "Billing Street").textAreaWrite(billing_street);
+        new TextArea(driver, "Shipping Street").textAreaWrite(shipping_street);
+
+
+    }
+
+    public void save() {
+        driver.findElement(By.cssSelector("[title=Save]")).click();
+    }
+}
